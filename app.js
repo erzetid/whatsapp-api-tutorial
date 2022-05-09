1a4cff11f0ddcaa3dc59d9231feb7d98b7b9e231
Muhammad Andri Fahrizal <fahrizalm14@gmail.com>
2022-05-10 04:37:03 +0700

    add cors

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const { body, validationResult } = require('express-validator');
 const socketIO = require('socket.io');
 const qrcode = require('qrcode');
 const http = require('http');
-const fs = require('fs');
 const { phoneNumberFormatter } = require('./helpers/formatter');
 const fileUpload = require('express-fileupload');
 
@@ -14,6 +13,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
+app.use(cors());
 app.use(express.json());
 app.use(
   express.urlencoded({
@@ -25,17 +25,6 @@ app.use(
     debug: true
   })
 );
-
-app.use(function (req, res, next) {
-  //Enabling CORS
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization'
-  );
-  next();
-});
 
 app.get('/', (req, res) => {
   res.sendFile('index.html', {
